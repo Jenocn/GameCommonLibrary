5f0c4062f4ba9b10c18fe22b41c8876dcff33fcd
Jenocn <544673717@qq.com>
2020-03-30 09:45:19 +0800

    消息中心增加一个自定义消息派发器的方法

--- Pattern/src/MessageCenter.cs
+++ Pattern/src/MessageCenter.cs
@@ -3,56 +3,78 @@
 	https://jenocn.github.io/
 */
 
+using System.Collections.Generic;
+
 namespace GCL.Pattern {
 
 	/// <summary>
 	/// 消息中心,一个全局消息派发器
 	/// </summary>
 	public static class MessageCenter {
-		private static MessageDispatcher _messageDispatcher = new MessageDispatcher();
 		public static MessageDispatcher messageDispatcher { get { return _messageDispatcher; } }
+		private static MessageDispatcher _messageDispatcher = new MessageDispatcher();
+		private static Dictionary<string, MessageDispatcher> _dispathcerDict = new Dictionary<string, MessageDispatcher>();
 
-		public static MessageDispatcher GetMessageDispatcher() {
+		/// <summary>
+		/// 派发队列中的消息
+		/// <para>若要使用Push功能则需要在一个Update中主动调用此方法</para>
+		/// </summary>
+		public static void OnDispatch() {
+			_messageDispatcher.OnDispatch();
+			foreach (var item in _dispathcerDict) {
+				item.Value.OnDispatch();
+			}
+		}
+
+		/// <summary>
+		/// 获取中心(全局)消息派发器
+		/// </summary>
+		public static MessageDispatcher GetCenterDispatcher() {
 			return _messageDispatcher;
 		}
 
 		/// <summary>
-		/// 添加消息监听者
+		/// 获取指定名称的自定义消息派发器
+		/// </summary>
+		public static MessageDispatcher GetCustomDispatcher(string name) {
+			MessageDispatcher ret = null;
+			if (!_dispathcerDict.TryGetValue(name, out ret)) {
+				ret = new MessageDispatcher();
+				_dispathcerDict.Add(name, ret);
+			}
+			return ret;
+		}
+
+		/// <summary>
+		/// 向中心派发器添加消息监听者
 		/// </summary>
 		public static void AddListener<T>(object obj, System.Action<T> func) where T : MessageBase<T> {
 			_messageDispatcher.AddListener(obj, func);
 		}
 
 		/// <summary>
-		/// 删除监听者
+		/// 从中心派发器删除监听者
 		/// </summary>
 		public static void RemoveListener<T>(object obj) where T : MessageBase<T> {
 			_messageDispatcher.RemoveListener<T>(obj);
 		}
 
 		/// <summary>
-		/// 立即发送消息
+		/// 中心派发器立即发送消息
 		/// </summary>
 		public static void Send(IMessage message) {
 			_messageDispatcher.Send(message);
 		}
 
 		/// <summary>
-		/// 入队消息,待OnDispathch时发送
+		/// 入队消息到中心派发器,待OnDispathch时发送
 		/// </summary>
 		public static void Push(IMessage message) {
 			_messageDispatcher.Push(message);
 		}
 
 		/// <summary>
-		/// 派发队列中的消息
-		/// </summary>
-		public static void OnDispatch() {
-			_messageDispatcher.OnDispatch();
-		}
-
-		/// <summary>
-		/// 清空消息和监听者
+		/// 清空中心派发器的消息和监听者
 		/// </summary>
 		public static void Clear() {
 			_messageDispatcher.Clear();
