1d6d8eee7553b639bef59fe0c0aa2be008a0c782
Jenocn <544673717@qq.com>
2020-08-19 23:08:39 +0800

    增加MathTool

---
+++ src/Base/MathTool.cs
@@ -0,0 +1,15 @@
+/*
+	By Jenocn
+	https://jenocn.github.io/
+*/
+
+namespace GCL.Base {
+	public static class MathTool {
+		// PI
+		public const float PI = 3.141593f;
+		// 弧度/每角度 (PI / 180)
+		public const float RADIAN_PER_ANGLE = PI / 180.0f;
+		// 角度/每弧度 (180 / PI)
+		public const float ANGLE_PER_RADIAN = 180.0f / PI;
+	}
+}
